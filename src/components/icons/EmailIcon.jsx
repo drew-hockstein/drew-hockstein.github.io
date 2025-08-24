84872b22c43e374f5aa3244c57fbb7bd7f663e43
drew-hockstein <64031198+drew-hockstein@users.noreply.github.com>
2025-08-23 19:27:57 -0600

    feat: add reusable icons

---
+++ src/components/icons/EmailIcon.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable react/prop-types */
+export default function EmailIcon({ className = 'w-6 h-6', ...props }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      className={className}
+      aria-hidden="true"
+      {...props}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M16 12l-4-4-4 4m8 0v6a2 2 0 01-2 2H6a2 2 0 01-2-2v-6m16 0V6a2 2 0 00-2-2H6a2 2 0 00-2 2v6"
+      />
+    </svg>
+  );
+}
